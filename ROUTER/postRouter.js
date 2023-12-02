cd82df48d8a147597ba7e775afdab9dade4266f2
Aalan A <aalansasonsingarayan@mail.com>
2023-12-02 12:25:09 +0530

    task2 commit

--- ROUTER/postRouter.js
+++ ROUTER/postRouter.js
@@ -2,18 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 router.route('/')
-    .get(require('../MIDDLEWARE/jwtVerify'),require('../CONTROLLERS/postController').getAllPosts)
-    .post(require('../MIDDLEWARE/jwtVerify'),require('../CONTROLLERS/postController').newPost)
+    .get(require('../CONTROLLERS/postController').getAllPosts)
+    .post(require('../CONTROLLERS/postController').newPost)
 
 router.route('/:id')
-    .get(require('../MIDDLEWARE/jwtVerify'),require('../CONTROLLERS/postController').getPost)
-    .put(require('../MIDDLEWARE/jwtVerify'),require('../CONTROLLERS/postController').updatePost)
-    .delete(require('../MIDDLEWARE/jwtVerify'),require('../CONTROLLERS/postController').deletePost)
+    .get(require('../CONTROLLERS/postController').getPost)
+    .put(require('../CONTROLLERS/postController').updatePost)
+    .delete(require('../CONTROLLERS/postController').deletePost)
 
 router.route('/:id/likes')
-    .post(require('../MIDDLEWARE/jwtVerify'),require('../CONTROLLERS/postController').addLikes)
+    .post(require('../CONTROLLERS/postController').addLikes)
 
 router.route('/:id/comments')
-    .post(require('../MIDDLEWARE/jwtVerify'),require('../CONTROLLERS/postController').addComments)
+    .post(require('../CONTROLLERS/postController').addComments)
 
 module.exports = router
